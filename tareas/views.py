cf4519ace4b523873029c7506eb06c804b8d395a
nfnakamura <nfn1991@hotmail.com>
2022-10-10 16:41:44 -0300

    creado el registro de usuario

--- tareas/views.py
+++ tareas/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'signup.html')
+
+
+def home(request):
+    return render(request, 'home.html', {
+        'form': UserCreationForm
+    })
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado satisfactoriamente')
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Las contraseñas no coinciden'
+        })
